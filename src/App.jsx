4452dc169c877d7a4f063216dbcf5d82c50732d4
Viktor.A <viktor.antonyshyn@gmail.com>
2023-06-30 14:58:39 +0200

    Added reset.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './reset.css'
 import MolecHappyUsers from './Moleculas/molec_HappyUsers'
 
 function App() {
