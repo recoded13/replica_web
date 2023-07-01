e84408586d453437ce3ab95510ce1828243840fd
Viktor.A <viktor.antonyshyn@gmail.com>
2023-07-01 13:19:45 +0200

    Created Counter. Works!!!

--- src/App.jsx
+++ src/App.jsx
@@ -8,40 +8,16 @@ import MolecIssuesSolved from './Moleculas/molec_IssuesSolved'
 import MolecGoodReviews from './Moleculas/molec_GoodReviews'
 import MolecCaseStudies from './Moleculas/molec_CaseStudies'
 import MolecOrdersReceived from './Moleculas/molec_ordersreceived'
+import Counter from './Components/counter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='counter'>
-        <MolecHappyUsers />
-        <MolecIssuesSolved />
-        <MolecGoodReviews />
-        <MolecCaseStudies />
-        <MolecOrdersReceived/>
-      </div>
-      <br /><br /><br /><br /><br />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Counter/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,9 +1,18 @@
+import MolecCaseStudies from '../Moleculas/molec_CaseStudies'
+import MolecGoodReviews from '../Moleculas/molec_GoodReviews'
 import MolecHappyUsers from '../Moleculas/molec_HappyUsers'
+import MolecIssuesSolved from '../Moleculas/molec_IssuesSolved'
+import MolecOrdersReceived from '../Moleculas/molec_ordersreceived'
+import './counter.css'
 
 const Counter = () => {
     return (
-        <div>
+        <div className='counter'>
             <MolecHappyUsers/>
+            <MolecIssuesSolved/>
+            <MolecGoodReviews/>
+            <MolecCaseStudies/>
+            <MolecOrdersReceived/>
         </div>
     )
 }
