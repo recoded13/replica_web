42b0e48da61578c8e74ef32ae5f7f141bb8ea770
Viktor.A <viktor.antonyshyn@gmail.com>
2023-07-01 13:24:21 +0200

    Cleared App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import './reset.css'
-import MolecHappyUsers from './Moleculas/molec_HappyUsers'
-import MolecIssuesSolved from './Moleculas/molec_IssuesSolved'
-import MolecGoodReviews from './Moleculas/molec_GoodReviews'
-import MolecCaseStudies from './Moleculas/molec_CaseStudies'
-import MolecOrdersReceived from './Moleculas/molec_ordersreceived'
 import Counter from './Components/counter'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
