3447df68f11a8023a6aa6f4a71d67faa3a0cc160
Viktor.A <viktor.antonyshyn@gmail.com>
2023-07-03 19:22:02 +0200

    Added page-4

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Counter from './Components/counter'
 import Counter1 from './Components/counter1'
 import Download_2botones from './Components/download_2botones'
 import TextAbovButDownL from './Atomos/text_above_but_downl'
+import TextAbovButDownL2 from './Atomos/text_above_but_downl2'
 
 
 function App() {
@@ -15,9 +16,15 @@ function App() {
         <h1 className='header'>Mobile App Landing Page</h1>
         <TextAbovButDownL />
         <Download_2botones />
-        <img className='header-iphone' src="public/Pics/header-iphone.png" alt="Pic header-iphone" />
+        <img className='header-iphone' src="/Pics/header-iphone.png" alt="Pic header-iphone" />
         <Counter1/>
+      </div>
+        <br />
+      <div className='page-4'>
         <Counter/>
+        <img className='download-iphone' src="/Pics/download-iphone.png" alt="Pic download-iphone" />
+        <TextAbovButDownL2 />
+        <Download_2botones />
       </div>
     </>
   )
---
+++ src/Atomos/text_above_but_downl2.jsx
@@ -0,0 +1,11 @@
+import '../Atomos/text_above_but_downl.css'
+
+const TextAbovButDownL2 = () => {
+    return (
+        <div>
+            <p className='texto'>Do you feel like you're wasting time with small stuff instead of working towards your goals? Start using Sync to organize your time and get a grip on your personal development</p>
+        </div>
+    )
+}
+
+export default TextAbovButDownL2;
